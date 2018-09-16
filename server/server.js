69b4a4b215a997c1d853ae4eb3da05cd68373fde
dinesh.kumawat <dinesh.kumawat@guidingstardigital.com>
2018-09-16 22:09:21 +0530

    private chat

--- server/server.js
+++ server/server.js
@@ -32,12 +32,19 @@ io.on('connect',(socket)=>{
     callback();
   });
   socket.on('createMessage',(message,callback)=>{
-    console.log('createMessage',message);
-    io.emit('newMessage',generateMessage(message.from,message.text));
+    var user = users.getUser(socket.id);
+    if(user && isRealString(message.text)){
+      io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
+    }
+
     callback();
   });
   socket.on('createLocationMessage',(coords)=>{
-    io.emit('newLocationMessage',generateLocationMessage('Location',coords.latitude,coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user && isRealString(user)){
+      io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+    }
+
   });
   socket.on('disconnect',()=>{
     console.log('Client Disconnected');
